b07d04fb6f6e36b6be1b5849671f04ff5e797725
lgb06 <21373216@buaa.edu.cn>
2026-01-22 22:26:40 +0800

    sh

--- run.py
+++ run.py
@@ -127,7 +127,7 @@ if __name__ == '__main__':
     parser.add_argument('--cls_mode', type=int, default=-1,
                         help='classes num mode')
     parser.add_argument('--data_demo', action='store_true', default=False,
-                        help='enable demo sampling (single mid-trial / mid-slice)')
+                        help='enable demo sampling (single mid-trial / mid-slice) specially for train')
 
 
     args = parser.parse_args()
